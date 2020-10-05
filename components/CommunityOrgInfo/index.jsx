dce3b9186eb8c6ee5b078c31a866b29eb528a7b3
Benjamin Gordon <benjamingordon@MacBook-Air.lan1>
2020-10-04 23:02:35 -0400

    update info so that boxes are formatted better

--- components/CommunityOrgInfo/index.jsx
+++ components/CommunityOrgInfo/index.jsx
@@ -1,21 +1,71 @@
 import React from 'react';
 import Link from 'next/link';
 
+const whiteBackgroundStyle = {backgroundColor: "white",color: "black",padding: "12px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "black", width: "92%",
+textAlign: "center"};
+
+const blackBackgroundStyle = {backgroundColor: "black",color: "white",padding: "12px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "white", width: "92%",
+textAlign: "center"};
+
+const whiteBackgroundLeftAlignStyle = {backgroundColor: "white",color: "black",padding: "12px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "black", width: "92%",
+textAlign: "left"};
+
+const TextDisplay = ({beforeText,afterText,text,style}) => (
+  <p style={style}>
+    {text ? `${beforeText+text+afterText}`: ''}{text ? <br /> : ''}
+  </p>
+);
+
+const TextDisplayArray = ({beforeText,afterText,text,style}) => (
+  <div style={style}>
+    {
+      text.map((value, index) => <p key={index}> {text[index] ? `${beforeText[index]+text[index]+afterText[index]}`: ''}{text[index] ? <br /> : ''}</p>)
+    }    
+  </div>
+);
+
+const ContactInfo = ({contactInfo}) => (  
+  <p style={{backgroundColor: "white",color: "black",padding: "12px",borderRadius: "25px",
+                border: "solid",borderWidth: "4px",borderColor: "black", width: "92%",
+                textAlign: "center"}}>    
+    
+    {contactInfo.includes("@") && `Contact Info: ${<a href={"mailto:"+contactInfo}>{contactInfo}</a>}`}{contactInfo && contactInfo !== "N/A" ? <br /> : ''}
+    {!contactInfo.includes("@")&& contactInfo.includes(".") && contactInfo !== "N/A"? `Contact Info: ${<a target= "_blank" href={"mailto:"+contactInfo}>{contactInfo}</a>}`: ''}{contactInfo && contactInfo !== "N/A" ? <br /> : ''}
+    {contactInfo.includes("-")&&!contactInfo.includes("@")&& !contactInfo.includes(".") && contactInfo !== "N/A"? `Contact Info: ${<a href={"tel:"+contactInfo}>{contactInfo}</a>}`: ''}{contactInfo && contactInfo !== "N/A" ? <br /> : ''}
+    {/* Phone Number: <a href={"tel:"+phone}>{phone}</a><br /> */}
+  </p>
+);
+
+const URL = ({url,style}) => (
+  <p style={style}>
+    URL: <a target= "_blank" href={url}>{url}</a><br />
+  </p>
+);
+
+
 export function CommunityOrgInfo ({ orgSite }) {
-  const { site_info } = orgSite;
+  //const site_info  = orgSite.site_info;
+  const {
+    name,
+    address,
+    type,
+    purpose,
+    neighborhoodAssociatedWith,
+    contactInfo,
+    website    
+  } = ((orgSite || {}).site_info || {});
   return (
-    <p>
-      {orgSite.name ? `Name: ${orgSite.name}` : ''}{orgSite.name ? <br /> : ''}
-      Address: {site_info.address} <br />
-      {site_info.type ? `Type: ${site_info.type}` : ''}{site_info.type ? <br /> : ''}
-      {site_info.purpose ? `Purpose: ${site_info.purpose}` : ''}{site_info.purpose ? <br /> : ''}
-      {site_info.neighborhoodAssociatedWith ? `Neighborhood(s) Associated With: ${site_info.neighborhoodAssociatedWith}` : ''}{site_info.neighborhoodAssociatedWith ? <br /> : ''}
-      {site_info.website ? `URL: `: ''}{site_info.website ? <a href={site_info.website}>{site_info.website}</a> : ''}{site_info.website ? <br /> : ''}
-      {site_info.contactInfo ? `Contact Info: `: ''}{site_info.contactInfo&&site_info.contactInfo.includes("@") ? <a href = {`mailto:${site_info.contactInfo}`}>{site_info.contactInfo}</a> : ''}
-      {site_info.contactInfo && !site_info.contactInfo.includes("@") ? <a href={site_info.contactInfo}>{site_info.contactInfo}</a> : ''}{site_info.contactInfo ? <br /> : ''}
-      <Link href="/community-orgs/[latLng]" as={`/community-orgs/${orgSite.coordinates.lat},${orgSite.coordinates.lng}`}>
-        <a>Link to this Info</a>
-      </Link>
-    </p>
+    <p>      
+      <TextDisplay beforeText={`Name: `} afterText = "" text={name} style ={blackBackgroundStyle} />
+      <TextDisplay beforeText={`Address: `} afterText = "" text={address} style={whiteBackgroundStyle} />
+      <TextDisplay beforeText={`Type: `} afterText = "" text={type} style={whiteBackgroundStyle}/>
+      <TextDisplay beforeText={`Purpose: `} afterText = "" text={purpose} style={whiteBackgroundLeftAlignStyle}/>
+      <TextDisplay beforeText={`Neighborhood(s): `} afterText = "" text={neighborhoodAssociatedWith} style={whiteBackgroundStyle}/>
+      <TextDisplay beforeText={`Contact Info: `} afterText = "" text={contactInfo} style={whiteBackgroundStyle}/>
+      <URL url = {website} style = {whiteBackgroundStyle} />
+      </p>
   );
 }
--- components/TestingSiteInfo/index.jsx
+++ components/TestingSiteInfo/index.jsx
@@ -2,11 +2,47 @@ import React from 'react';
 import Link from 'next/link';
 import { startCase } from 'lodash';
 
-const HourDisplay = ({ day, dayHours }) => (
-  <>
-    {day}: {dayHours}<br />
-  </>
+const whiteBackgroundStyle = {backgroundColor: "white",color: "black",padding: "12px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "black", width: "92%",
+textAlign: "center"};
+
+const blackBackgroundStyle = {backgroundColor: "black",color: "white",padding: "12px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "white", width: "92%",
+textAlign: "center"};
+
+const whiteBackgroundLeftAlignStyle = {backgroundColor: "white",color: "black",padding: "12px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "black", width: "92%",
+textAlign: "left"};
+
+const whiteBackgroundArray = {backgroundColor: "white",color: "black",padding: "0px",borderRadius: "25px",
+border: "solid",borderWidth: "4px",borderColor: "black", width: "92%",
+textAlign: "center"};
+
+const TextDisplay = ({beforeText,afterText,text,style}) => (
+  <p style={style}>
+    {text ? `${beforeText+text+afterText}`: ''}{text ? <br /> : ''}
+  </p>
 );
+const TextDisplayArray = ({beforeText,afterText,text,style}) => (
+  <div style={style}>
+    {
+      text.map((value, index) => <p key={index}> {text[index] ? `${beforeText[index]+text[index]+afterText[index]}`: ''}{text[index] ? <br /> : ''}</p>)
+    }    
+  </div>
+);
+
+const Phone = ({phone,style}) => (
+  <p style={style}>
+    Phone Number: <a href={"tel:"+phone}>{phone}</a><br />
+  </p>
+);
+
+const URL = ({url,style}) => (
+  <p style={style}>
+    URL: <a target= "_blank" href={url}>{url}</a><br />
+  </p>
+);
+
 
 export function TestingSiteInfo({ site }) {
   const {
@@ -21,35 +57,16 @@ export function TestingSiteInfo({ site }) {
   } = ((site || {}).site_info || {});
   return (
     <p >
-      {name ? `Name: ${name}` : ''}{name ? <br /> : ''}
-      Address: {address} <br />
-      {phone && (
-        <>          
-          Phone Number: <a href={"tel:"+phone}>{phone}</a><br />
-        </>
-      )}
-      You are {appointment_required ? 'required' : 'not required'} to have an appointment to get tested.<br />
-      This testing site {antibody_testing ? 'provides' : 'does not provide'} antibody testing. <br />
-      Hours: <br />
-      {hours.sunday && <HourDisplay day="Sunday" dayHours={hours.sunday} />}
-      {hours.monday && <HourDisplay day="Monday" dayHours={hours.monday} />}
-      {hours.tuesday && <HourDisplay day="Tuesday" dayHours={hours.tuesday} />}
-      {hours.wednesday && <HourDisplay day="Wednesday" dayHours={hours.wednesday} />}
-      {hours.thursday && <HourDisplay day="Thursday" dayHours={hours.thursday} />}
-      {hours.friday && <HourDisplay day="Friday" dayHours={hours.friday} />}
-      {hours.saturday && <HourDisplay day="Saturday" dayHours={hours.saturday} />}
-      {additional_info ? `Additional Information: ${additional_info}` : ''}{additional_info ? <br /> : ''}
-      {provider_url && (
-        <>
-          URL: <a href={provider_url}>{provider_url}</a><br />
-        </>
-      )}
-      <Link
-        href={site.pageLink.href}
-        as={site.pageLink.as}
-      >
-          <a>Link to this Info</a>
-      </Link>
+      <TextDisplay beforeText={`Name: `} afterText = "" text={name} style={blackBackgroundStyle} />
+      <TextDisplay beforeText ={`Address: `} afterText = "" text={address} style={whiteBackgroundStyle}/>
+      <Phone phone={phone} style = {whiteBackgroundStyle}/>
+      <TextDisplay beforeText ={`You are `} afterText = " to have an appointment to get tested." text={appointment_required ? 'required' : 'not required'} style={whiteBackgroundStyle}/>
+      <TextDisplay beforeText ={`This testing site `} afterText = " antibody testing." text={antibody_testing ? 'provides' : 'does not provide'} style={whiteBackgroundStyle}/>
+      <TextDisplayArray beforeText={["Sunday: ","Monday: ","Tuesday: ","Wednesday: ","Thursday: ","Friday: ","Saturday: "]}
+        text={[hours.sunday,hours.monday,hours.tuesday,hours.wednesday,hours.thursday,hours.friday,hours.saturday]}
+        afterText={["","","","","","",""]} style={whiteBackgroundArray}/>
+      <TextDisplay beforeText ={`Additional Information: `} afterText = "" text={additional_info} style={whiteBackgroundStyle}/>      
+      <URL url = {provider_url} style = {whiteBackgroundStyle} />
     </p>
   );
 }
